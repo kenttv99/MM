8598c6940a51862d48dab9c514f16e3de07f89f5
kenttv11 <kenttv11@yandex.ru>
2025-03-16 16:26:34 +0300

    add tables

--- backend/database/user_db.py
+++ backend/database/user_db.py
@@ -52,12 +52,99 @@ class User(Base):
     created_at = Column(TIMESTAMP, default=datetime.utcnow)
     updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
     
-    
+    # Связь с параметрами пользователя
+    params = relationship("UserParams", uselist=False, back_populates="user")
+
+
 class UserParams(Base):
-    __tablename__ = "users_parametrs"
+    __tablename__ = "users_params"
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     max_card_amount = Column(DECIMAL(20, 8), nullable=False)
     max_photo_program_amount = Column(DECIMAL(20, 8), nullable=False)
     max_video_program_amount = Column(DECIMAL(20, 8), nullable=False)
+    
+    # Обратная ссылка на пользователя
+    user = relationship("User", back_populates="params")
+    
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    description = Column(Text)
+    start_date = Column(TIMESTAMP, nullable=False)
+    end_date = Column(TIMESTAMP)
+    location = Column(String(255))
+    image_url = Column(String(255))  # Ссылка на обложку мероприятия
+    price = Column(DECIMAL(20, 8), nullable=False)  # Цена билета
+    published = Column(Boolean, default=False)  # Публикуемое событие
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
+    updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+class TicketType(Base):
+    __tablename__ = "ticket_types"
+
+    id = Column(Integer, primary_key=True, index=True)
+    event_id = Column(Integer, ForeignKey("events.id"))
+    name = Column(String(255), nullable=False)  # Название типа билета (например, "Стандарт", "Премиум")
+    price = Column(DECIMAL(20, 8), nullable=False)  # Цена билета
+    available_quantity = Column(Integer, nullable=False)  # Количество доступных билетов данного типа
+    sold_quantity = Column(Integer, default=0)  # Количество проданных билетов данного типа
+    free_registration = Column(Boolean, default=False)  # Возможность бесплатной регистрации
+    
+class Registration(Base):
+    __tablename__ = "registrations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    event_id = Column(Integer, ForeignKey("events.id"))
+    ticket_type_id = Column(Integer, ForeignKey("ticket_types.id"))
+    ticket_number = Column(String(255))  # Номер билета
+    payment_status = Column(Boolean, default=False)  # Статус оплаты
+    amount_paid = Column(DECIMAL(20, 8))  # Сумма платежа
+    status = Column(String(30), default='pending')  # Статусы: pending, approved, rejected
+    submission_time = Column(TIMESTAMP, default=datetime.utcnow)
+
+
+async def init_db():
+    """
+    Инициализация базы данных.
+    """
+    try:
+        async with engine.begin() as conn:
+            # Создание всех таблиц
+            await conn.run_sync(Base.metadata.create_all)
+            print("Таблицы успешно созданы.")
+    except Exception as e:
+        print(f"Произошла ошибка при инициализации базы данных: {e}")
+    finally:
+        await engine.dispose()
+
+
+@asynccontextmanager
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        async with session.begin():
+            try:
+                yield session
+            except Exception:
+                await session.rollback()
+                raise
+            finally:
+                await session.close()
+
+
+# Dependency для FastAPI
+async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
